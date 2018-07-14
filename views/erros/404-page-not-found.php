cc940bb77f8884f7ba602658fc36491c6c41dae4
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2018-07-14 12:36:58 -0300

    Rename views/page-not-found/ to views/erros/

---
+++ views/erros/404-page-not-found.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Page not fould</title>
+
+    <style>
+    h2 {
+        font-family:arial;
+        display:block;
+        text-align:center;
+    }
+    </style>
+
+</head>
+<body>
+    <!--(massage_404) This massage show you the type of the error about page not found-->
+    <h2><?php echo $massage404;?></h2>
+</body>
+</html>
