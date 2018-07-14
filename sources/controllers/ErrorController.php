cee8d1d99acd144c010517597b46c278babc383b
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2018-07-14 16:16:30 -0300

    Correct view method in ErrorController.php

--- sources/controllers/ErrorController.php
+++ sources/controllers/ErrorController.php
@@ -4,16 +4,16 @@ class ErrorController extends BaseController
 {
 	public function controllerNotFound() {
 		$message = 'controller not found';
-		return $this->view->make('errors.404-page-not-found', compact('title'));
+		return $this->view('errors.404-page-not-found', compact('title'));
 	}
 
 	public function methodNotFound() {
 		$message = 'method not found';
-		return $this->view->make('errors.404-page-not-found', compact('title'));
+		return $this->view('errors.404-page-not-found', compact('title'));
 	}
 
 	public function viewNotFound() {
 		$message = 'view not found';
-		return $this->view->make('errors.404-page-not-found', compact('title'));
+		return $this->view('errors.404-page-not-found', compact('title'));
 	}
 }
