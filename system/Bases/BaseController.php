c0721084d7e06f436b26d062b8a3af3199e470cb
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2018-07-14 16:46:17 -0300

    Correct some informations in BaseController after importation of code

--- system/Bases/BaseController.php
+++ system/Bases/BaseController.php
@@ -57,7 +57,7 @@ class BaseController
     protected $data = array();
 
     # to receive the name of the layout that will be used in the controller
-    protected $layoutName = false;
+    protected $layout = 'default-layout';
 
     # to receive the name of the views that will be used into the layout
     public $content = null;
@@ -68,10 +68,9 @@ class BaseController
     {
         if (array_key_exists($name, $this->includeFiles)) {
             return include($this->includeFiles[$name]);
-        } else {
-            echo "<b>(In View) {$name}</b>: <font color='red'> This file not exist, you should verify the name and path of the file.";
-            exit;
-        }
+		}
+		echo "<b>(In View) {$name}</b>: <font color='red'> This file not exist, you should verify the name and path of the file.";
+		exit;
     }
 
     # Return the array of the values that will be used in the views
@@ -81,61 +80,57 @@ class BaseController
     }
 
     # Receive the name and the path of the layout
-    public function layout($layout_name = false)
+    public function layout($layout = false)
     {
-        $this->layout_name = $layout_name;
+		$this->layout = $layout;
+		return $this;
     }
 
     /**
     * Render the view used in the controller
     *
-    * @param view_name : string : Name of view and the name of the folder of the view
+    * @param view : string : Name of view and the name of the folder of the view
     * @param data : mixed : Values that will be passed to the view
     */
-
-    public function view($view_name, $data = false)
+    public function view($view, $data = false)
     {
         if ($data) {
             # Set in array the values passed to view
             foreach ($data as $key => $items) {
                 $this->data[$key] = $items;
             }
-        }
+		}
 
         # Transforming the operator '.' in operator '='
-        $view_name = self::toSlash($view_name);
+		$view = StringHelper::toSlash($view);
+		$view = __DIR__ .'/../../'. Route::$viewDir . "{$view}.php";
 
         # Passing the values to be used into the views
         foreach ($this->getData() as $key => $itens) {
             $$key = $itens;
         }
 
-        # Verify if exist the view file
-        if (file_exists("views/{$view_name}.php")) {
+		# Verify if exist the view file
+        if (!file_exists($view)) {
+			$view = __DIR__ .'/../../'. Route::$viewDir . "errors/404-page-not-found.php";;
+		}
 
-            # Passing the name and path of the views files
-            $this->content = "views/{$view_name}.php";
+		# Passing the name and path of the views files
+		$this->content = $view;
 
-            # Verify if the method are using a layout
-            if ( ! $this->layout_name == null) {
-
-                # Verify if the layout exist in the layout folder
-                if (file_exists("layouts/{$this->layout_name}.php")) {
-
-                    # Include the layout
-                    require_once("layouts/{$this->layout_name}.php");
-
-                } else {
-                    echo 'This layout not exist in the layout folder';
-                }
-
-            } else {
-                require_once("views/{$view_name}.php");
-            }
-
-        } else {
-            echo 'This View not Exist in this folder';
-        }
+		# Verify if the method are using a layout
+		if ($this->layout !== null) {
+			$this->layout = __DIR__ ."/../../sources/layouts/{$this->layout}.php";
+			# Verify if the layout exist in the layout folder
+			if (file_exists($this->layout)) {
+				# Include the layout
+				$this->content = $view;
+				require_once($this->layout);
+				exit;
+			}
+		}
+		$view = __DIR__ .'/../../'. Route::$viewDir . "errors/404-page-not-found.php";
+		require_once($view);
     }
 
     public function withFiles($key_word, $name)
