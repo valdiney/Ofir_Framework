ffb4d5afd049ead66ffae5df91433304f3b2bf89
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2018-07-14 15:53:48 -0300

    Create ErrorController

--- sources/controllers/ErrorController.php
+++ sources/controllers/ErrorController.php
@@ -2,5 +2,18 @@
 
 class HomeController extends BaseController
 {
+	public function controllerNotFound() {
+		$message = 'controller not found';
+		return $this->view->make('errors.404-page-not-found', compact('title'));
+	}
 
+	public function methodNotFound() {
+		$message = 'method not found';
+		return $this->view->make('errors.404-page-not-found', compact('title'));
+	}
+
+	public function viewNotFound() {
+		$message = 'view not found';
+		return $this->view->make('errors.404-page-not-found', compact('title'));
+	}
 }
