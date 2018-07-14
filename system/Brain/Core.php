58187e3de18041d7b937136ddfbbcf13e4ccc9ed
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2018-07-14 16:44:24 -0300

    Add initialization of the system in Brain/Core

--- system/Brain/Core.php
+++ system/Brain/Core.php
@@ -2,5 +2,11 @@
 
 class Core
 {
-	public function init() {}
+	public static function init() {
+		$controller = Route::$controller;
+		$method     = Route::$method;
+		$controller = new Route::$controller;
+		$controller->$method();
+		$controller->finalyze();
+	}
 }
