bfc2b2c4d6aa9a7b7d798fe0656dcf7fb06e84fa
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2018-07-14 12:38:38 -0300

    Add initial info in Routes/Route class

--- system/Routes/Route.php
+++ system/Routes/Route.php
@@ -4,5 +4,22 @@ namespace Ofir\Routes;
 
 class Route
 {
+	protected $controller = null;
+	protected $method     = null;
+
+	protected $viewNotFound = 'views/erros/404-page-not-found.php';
+
+	protected $messages = [
+		1 => "The name of the Controller or the name of the Method can be wrong or not exist.",
+		2 => "The Controller '%s'  not exist.",
+		3 => "The Method '%s' not exist on Controller '%s'.",
+	];
+
+	/**
+	 * This method is find actual route.
+	 *
+	 * @return void
+	 */
 	public static function init() {}
+
 }
