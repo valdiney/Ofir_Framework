b72322f9cfe013961bd2cff56c04814e625996c4
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2018-07-16 19:10:11 -0300

    Edits class Core
    - Corrects somethings that was wrong about Route class
    - Closes #21 - Class has been sees

--- system/Brain/Core.php
+++ system/Brain/Core.php
@@ -2,11 +2,18 @@
 
 class Core
 {
+    /**
+     * Initializing the Core of Ofir.
+     * That class get the actual route and
+     * initialize the controller and the method founded.
+     *
+     * @return void
+     */
     public static function init() {
         $controller = Route::$controller;
         $method     = Route::$method;
-        $controller = new Route::$controller;
-        $controller->$method();
+        $controller = new $controller;
+        $controller->{$method}();
         $controller->finalyze();
     }
 }
