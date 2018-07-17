24f1fce430393d6d7b9aa5368ba593233d7042ce
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2018-07-17 19:20:28 -0300

    Modifies the Hash class
    - Changes the method hash to `password`
    - Adds two methods: `make` and `verify`
    - Closes #27

--- system/Helpers/Hash.php
+++ system/Helpers/Hash.php
@@ -1,10 +1,29 @@
 <?php
 
+/**
+ * Give a hashed string.
+ * For now, this class is hashing by function 'password'.
+ */
 class Hash
 {
-    public static function make($password = null) {
-        $salt = "dfddfdfddfd;dfdfd45654;df45541254sdsdw";
-        $unic_salt = "asweq451";
-        return sha1($password.$salt.$unic_salt);
+    /**
+     * Make a hashed string of 60 characters
+     *
+     * @param String $value
+     * @return String
+     */
+    public static function make(String $value): String {
+        return password_hash($value, PASSWORD_DEFAULT);
+    }
+
+    /**
+     * Verify if the value is equals a hashed string
+     *
+     * @param String $value
+     * @param String $verification
+     * @return Bool
+     */
+    public static function verify(String $value, String $verification): Bool {
+        return password_verify($value, $verification);
     }
 }
