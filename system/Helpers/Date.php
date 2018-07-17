6d49d2bb0dff8d38a719b893a37ff5d0d4045546
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2018-07-17 19:57:46 -0300

    Corrects class Date
    - Adds more options
    - Configures by `.env` file

--- system/Helpers/Date.php
+++ system/Helpers/Date.php
@@ -5,54 +5,57 @@
 
 class Date
 {
-    public static function dateFormat($date = null) {
-        # Converting date to USA standard
-        if (strpos($date, "/")) {
-            $date = explode("/", $date);
-            return $date[2] . "-" . $date[1] . "-" . $date[0];
-        }
+    protected $date = null;
+    protected $type = null;
 
-        # Converting date to BR standard
-        if (strpos($date, "-")) {
-            $date = explode("-", $date);
-            return $date[2] . "/" . $date[1] . "/" . $date[0];
-        }
+    /**
+     * Initialize with two optionals params
+     * @param String $date
+     * @param String $type
+     */
+    public function __construct(String $date=null, String $type='br') {
+        $date = $date? $date: time();
+        $this->date = new DateTime($date);
+        $this->type = strtoupper($type);
     }
 
-    public static function dateNow($format = null) {
-        $format = trim(strtolower($format));
-        $format = strtoupper($format);
-
-        if ($format == 'BR') {
-            return Date('d/m/Y');
-        }
-
-        if ($format == 'USA' or 'EUA') {
-            return Date('Y-m-d');
-        }
+    /**
+     * Get the date formated
+     * @param String $format
+     * @return void
+     */
+    public function format(String $format=null) {
+        $format = $format? $format: ($type=='BR'? 'm/d/Y': 'm-d-Y');
+        return $this->date->format($format);
     }
 
-    # This method is used to return a date time, the parameters represent 'continent' and 'city'
-    # to be put in time zone
-    public static function dateTime($format = false, $your_date = false, $continent = 'America', $city = 'Araguaina') {
-        date_default_timezone_set("{$continent}/{$city}");
-        $date = date("Y-m-d H:i:s");
-
-        if ($format == null) {
-            return $date;
-        }
-
-        if ($your_date != null) {
-            return date($format, strtotime($your_date));
-        }
-
-        return date($format, strtotime($date));
+    public function now() {
+        return $format==='BR'? date('d/m/Y'): date('m-d-Y');
     }
 
-    # This method is used to return the time, the parameters represent 'continent' and 'city'
-    # to be put in time zone
-    public static function hour($continent = 'America', $city = 'Araguaina') {
-        date_default_timezone_set("{$continent}/{$city}");
-        return date("H:i:s");
+    /**
+     * Configure a new timezone
+     *
+     * @param String $continent
+     * @param String $city
+     * @return $this
+     */
+    public function timezone(String $continent, String $city) {
+        $timezone = "{$continent}/{$city}";
+        $this->date = new Datetime($this->date, new DateTimeZone($timezone));
+        return $this;
+    }
+
+    /**
+     * This method is used to return the time, the parameters represent 'continent' and 'city'
+     * to be put in timezone
+     * @param String $continent
+     * @param String $city
+     * @return String
+     */
+    public function hour(String $continent, String $city) {
+        $timezone = "{$continent}/{$city}";
+        $this->date = new Datetime($this->date, new DateTimeZone($timezone));
+        return $this->date->format('H:i:s');
     }
 }
